63d6d70e8a53234e897bdcad1c41398b0a501654
LT_Naive <60535128+w-game@users.noreply.github.com>
2022-12-14 22:33:07 +0900

    fix bug

--- Assets/Scripts/UI/Common/ViewStack/TopStack.cs
+++ Assets/Scripts/UI/Common/ViewStack/TopStack.cs
@@ -20,6 +20,7 @@ namespace UI
             {
                 splash.gameObject.SetActive(false);
             });
+            AdManager.Instance.Init();
         }
 
         public void ShowToast(string msg)
