7c0ec74f08c2e1faed089af476ea1295dc1dfbdd
LT_Naive <60535128+w-game@users.noreply.github.com>
2022-12-29 07:12:23 +0900

    多语言补充 & fix bug

--- Assets/Scripts/GameMode/Blocks/NormalBlock.cs
+++ Assets/Scripts/GameMode/Blocks/NormalBlock.cs
@@ -8,5 +8,34 @@ namespace Blocks
         {
             Pattern = color;
         }
+
+        public override void ShowSpecialIcon(Sprite sprite, bool changeColor)
+        {
+            SpecialIcon.sprite = sprite;
+            SpecialIcon.gameObject.SetActive(true);
+            
+            if (changeColor)
+            {
+                var color = SpecialIcon.color;
+                if (color.r + color.g + color.b < 1.5f)
+                {
+                    var c = Pattern * 1.5f;
+                    c.a = 1;
+                    SpecialIcon.color = c;
+                }
+                else
+                {
+                    var c = Pattern * 1.5f;
+                    c.a = 1;
+                    SpecialIcon.color = c;
+                }
+            }
+        }
+
+        public override void HideSpecialIcon()
+        {
+            SpecialIcon.gameObject.SetActive(false);
+            SpecialIcon.color = Color.white;
+        }
     }
 }
