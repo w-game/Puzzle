c3f938b5d34b4700dbbe3d4c7d6d9a77add6a01e
LT_Naive <60535128+w-game@users.noreply.github.com>
2022-12-29 07:12:23 +0900

    多语言补充 & fix bug

--- Assets/Scripts/GameMode/Blocks/ShadowBlock.cs
+++ Assets/Scripts/GameMode/Blocks/ShadowBlock.cs
@@ -16,34 +16,5 @@ namespace Blocks
             Pattern = BlockPattern;
             SetSpecialIcon("Textures/shadow_block", Color.white);
         }
-
-        public override void ShowSpecialIcon(Sprite sprite, bool changeColor)
-        {
-            SpecialIcon.sprite = sprite;
-            SpecialIcon.gameObject.SetActive(true);
-            
-            if (changeColor)
-            {
-                var color = SpecialIcon.color;
-                if (color.r + color.g + color.b < 1.5f)
-                {
-                    var c = Pattern * 1.5f;
-                    c.a = 1;
-                    SpecialIcon.color = c;
-                }
-                else
-                {
-                    var c = Pattern * 1.5f;
-                    c.a = 1;
-                    SpecialIcon.color = c;
-                }
-            }
-        }
-
-        public override void HideSpecialIcon()
-        {
-            SpecialIcon.gameObject.SetActive(false);
-            SpecialIcon.color = Color.white;
-        }
     }
 }
